ca6df6fad0941eb4903589ea4416eaf11b1d4d23
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-08-12 12:15:06 +0200

    fix(editing gateway configs): make request to endpoint functional

--- src/gateways/useUpdateGenericGatewayMutation.js
+++ src/gateways/useUpdateGenericGatewayMutation.js
@@ -1,8 +1,10 @@
-import { useDataMutation } from '@dhis2/app-runtime'
+import { useState } from 'react'
+import { useDataEngine } from '@dhis2/app-runtime'
 
 /**
- * @TODO(request params):
- *   `messageParameter` & `recipientParameter` do not exist in the api
+ * @TODO:
+ *   * `messageParameter` & `recipientParameter` do not exist in the api
+ *   * "Request method 'PUT' not supported"
  */
 
 /**
@@ -42,11 +44,44 @@ const updateDataFromVariables = ({
     parameters,
 })
 
+/**
+ * @TODO
+ *   * Create jira issue: Make `useDataMutation` accept function for "id"
+ */
 export const UPDATE_GENERIC_GATEWAY_MUTATION = {
     resource: 'gateways',
+    id: '', // replace me dynamically
     type: 'replace',
     data: updateDataFromVariables,
 }
 
-export const useUpdateGenericGatewayMutation = () =>
-    useDataMutation(UPDATE_GENERIC_GATEWAY_MUTATION)
+export const useUpdateGenericGatewayMutation = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const engine = useDataEngine()
+
+    const doFetch = variables => {
+        setLoading(true)
+        setError(null)
+
+        const query = {
+            ...UPDATE_GENERIC_GATEWAY_MUTATION,
+            id: variables.id,
+        }
+
+        return engine
+            .mutate(query, { variables })
+            .then(data => {
+                setLoading(false)
+                return data
+            })
+            .catch(error => {
+                setLoading(false)
+                setError(error)
+
+                throw error
+            })
+    }
+
+    return [doFetch, { loading, error }]
+}
