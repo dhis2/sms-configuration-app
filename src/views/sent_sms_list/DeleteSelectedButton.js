ec216139a02f98019faf216d6602418cff7d71ef
ismay <ismay@ismaywolff.nl>
2020-08-19 12:14:56 +0200

    feat: add selected message deletion functionality

--- src/views/sent_sms_list/DeleteSelectedButton.js
+++ src/views/sent_sms_list/DeleteSelectedButton.js
@@ -1,19 +1,60 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Button } from '@dhis2/ui'
 import { PropTypes } from '@dhis2/prop-types'
+import { useDataEngine } from '@dhis2/app-runtime'
 import i18n from '../../locales'
+import RefetchSms from './RefetchSms'
 
-const DeleteSelectedButton = ({ disabled, onClick }) => {
+const DeleteSelectedButton = ({ selected }) => {
+    const refetch = useContext(RefetchSms)
+    const [loading, setLoading] = useState(false)
+    const disabled = selected.length === 0
+
+    // We get the ids as numbers from the backend
+    const ids = selected.map(id => id.toString()).join(',')
+
+    /**
+     * FIXME: Using the data engine directly since the data mutation doesn't seem to
+     * support this pattern. It is an uncommon pattern, so using the engine directly
+     * seems like the best solution.
+     */
+    const engine = useDataEngine()
+    const mutation = {
+        resource: 'sms/outbound/message',
+        type: 'delete',
+        params: {
+            ids,
+        },
+    }
+
+    const onClick = () => {
+        setLoading(true)
+        engine
+            .mutate(mutation)
+            .then(() => {
+                setLoading(false)
+                refetch()
+            })
+            .catch(() => {
+                setLoading(false)
+                console.log('should handle error')
+            })
+    }
+
     return (
-        <Button small destructive disabled={disabled} onClick={onClick}>
+        <Button
+            small
+            destructive
+            disabled={disabled || loading}
+            onClick={onClick}
+        >
             {i18n.t('Delete selected sms')}
         </Button>
     )
 }
 
 DeleteSelectedButton.propTypes = {
-    disabled: PropTypes.bool.isRequired,
-    onClick: PropTypes.func.isRequired,
+    selected: PropTypes.arrayOf(PropTypes.number).isRequired,
 }
 
 export default DeleteSelectedButton
