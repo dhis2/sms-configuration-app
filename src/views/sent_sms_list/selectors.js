865aa4a697835c004a8d15db8c85f06e93ca4506
ismay <ismay@ismaywolff.nl>
2020-08-19 17:00:49 +0200

    fix: improve select all checkbox behaviour

--- src/views/sent_sms_list/selectors.js
+++ src/views/sent_sms_list/selectors.js
@@ -2,5 +2,16 @@
 export const getAllIds = messages => messages.map(message => message.messageId)
 
 // Returns true if all messages are selected, false if not
-export const getAllSelected = (allIds, selected) =>
-    allIds.every(id => selected.includes(id))
+export const getAllSelected = (allIds, selected) => {
+    // If there are no ids there's nothing to select
+    const noIds = allIds.length === 0
+
+    // If the lengths don't match we're assuming that they're not all selected (assuming no duplicate ids)
+    const noLengthMatch = allIds.length !== selected.length
+
+    if (noIds || noLengthMatch) {
+        return false
+    }
+
+    return allIds.every(id => selected.includes(id))
+}
