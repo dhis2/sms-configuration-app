fbade75baf5573cce7e5b7209c477b69ff3dca75
ismay <ismay@ismaywolff.nl>
2020-08-25 15:22:48 +0200

    refactor: use data mutation for single delete

--- src/views/sent_sms_list/DeleteButton.js
+++ src/views/sent_sms_list/DeleteButton.js
@@ -1,51 +1,39 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { Button } from '@dhis2/ui'
 import { PropTypes } from '@dhis2/prop-types'
-import { useDataEngine } from '@dhis2/app-runtime'
+import { useDataMutation } from '@dhis2/app-runtime'
 import i18n from '../../locales'
 import { AlertContext } from '../../notifications'
 import RefetchSms from './RefetchSms'
 
+const mutation = {
+    resource: 'sms/outbound',
+    params: ({ ids }) => ({ ids }),
+    type: 'delete',
+}
+
 const DeleteButton = ({ id, cleanSelected }) => {
     const { addAlert } = useContext(AlertContext)
     const { refetch } = useContext(RefetchSms)
-    const [loading, setLoading] = useState(false)
-
-    /**
-     * FIXME: Using the data engine directly since the data mutation doesn't seem to
-     * support this pattern. It is an uncommon pattern, so using the engine directly
-     * seems like the best solution.
-     */
-    const engine = useDataEngine()
-    const mutation = {
-        resource: 'sms/outbound',
-        type: 'delete',
-        params: {
+    const [mutate, { loading }] = useDataMutation(mutation, {
+        onComplete: () => {
+            cleanSelected(id)
+            refetch()
+        },
+        onError: error => {
+            addAlert({ type: 'critical', message: error.message })
+        },
+        variables: {
             ids: id,
         },
-    }
-
-    const onClick = () => {
-        setLoading(true)
-        engine
-            .mutate(mutation)
-            .then(() => {
-                setLoading(false)
-                cleanSelected(id)
-                refetch()
-            })
-            .catch(error => {
-                setLoading(false)
-                addAlert({ type: 'critical', message: error.message })
-            })
-    }
+    })
 
     /**
      * We're disabling the button when fetching instead of rendering a spinner, since rendering
      * a spinner results in the whole table jumping and for a short fetch that looks very buggy
      */
     return (
-        <Button small destructive onClick={onClick} disabled={loading}>
+        <Button small destructive onClick={mutate} disabled={loading}>
             {i18n.t('Delete')}
         </Button>
     )
