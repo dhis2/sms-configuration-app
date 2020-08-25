117e2e23c343306de62fc758bd20e7036be97f09
ismay <ismay@ismaywolff.nl>
2020-08-25 15:45:04 +0200

    refactor: use data mutation for multiple delete

--- src/views/sent_sms_list/DeleteSelectedButton.js
+++ src/views/sent_sms_list/DeleteSelectedButton.js
@@ -1,52 +1,38 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { Button } from '@dhis2/ui'
 import { PropTypes } from '@dhis2/prop-types'
-import { useDataEngine } from '@dhis2/app-runtime'
+import { useDataMutation } from '@dhis2/app-runtime'
 import i18n from '../../locales'
 import { AlertContext } from '../../notifications'
 import RefetchSms from './RefetchSms'
 
+const mutation = {
+    resource: 'sms/outbound',
+    params: ({ ids }) => ({ ids }),
+    type: 'delete',
+}
+
 const DeleteSelectedButton = ({ selected }) => {
-    const { addAlert } = useContext(AlertContext)
-    const { refetchAndClear } = useContext(RefetchSms)
-    const [loading, setLoading] = useState(false)
     const disabled = selected.length === 0
     const ids = selected.join(',')
 
-    /**
-     * FIXME: Using the data engine directly since the data mutation doesn't seem to
-     * support this pattern. It is an uncommon pattern, so using the engine directly
-     * seems like the best solution.
-     */
-    const engine = useDataEngine()
-    const mutation = {
-        resource: 'sms/outbound',
-        type: 'delete',
-        params: {
-            ids,
+    const { addAlert } = useContext(AlertContext)
+    const { refetchAndClear } = useContext(RefetchSms)
+    const [mutate, { loading }] = useDataMutation(mutation, {
+        onComplete: () => {
+            refetchAndClear()
         },
-    }
-
-    const onClick = () => {
-        setLoading(true)
-        engine
-            .mutate(mutation)
-            .then(() => {
-                setLoading(false)
-                refetchAndClear()
-            })
-            .catch(error => {
-                setLoading(false)
-                addAlert({ type: 'critical', message: error.message })
-            })
-    }
+        onError: error => {
+            addAlert({ type: 'critical', message: error.message })
+        },
+    })
 
     return (
         <Button
             small
             destructive
             disabled={disabled || loading}
-            onClick={onClick}
+            onClick={() => mutate({ ids })}
         >
             {i18n.t('Delete selected SMSes')}
         </Button>
