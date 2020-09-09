c3bc10bbeca59ce6a63e25bd9f7778f594a2e8bc
ismay <ismay@ismaywolff.nl>
2020-09-09 09:37:10 +0200

    fix: use hash router

--- src/views/NoMatch.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { dataTest } from '../dataTest'
-
-export const NoMatch = () => (
-    <div data-test={dataTest('views-nomatch')}>
-        <h1>404</h1>
-    </div>
-)
