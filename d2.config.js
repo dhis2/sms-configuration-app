56a174c08c9d543ed81fb4c31e822803f38daf4f
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-09-10 13:03:35 +0200

    feat: upgrade platform, set coreApp back to true

--- d2.config.js
+++ d2.config.js
@@ -2,6 +2,8 @@ const config = {
     type: 'app',
     name: 'sms-configuration',
     title: 'SMS Configuration',
+    description: 'Configure SMS gateways and manage SMS messages',
+    coreApp: true,
 
     entryPoints: {
         app: './src/App',
