765e88e244a93e7c33201ceb9846c9d4982264e4
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-09-23 11:08:58 +0200

    fix(edit sms commands): enable submit btn when removing a special char
    Fixes DHIS2-9591

--- src/smsCommand/SaveCommandButton.js
+++ src/smsCommand/SaveCommandButton.js
@@ -1,4 +1,5 @@
 import { Button, CircularLoader, ReactFinalForm } from '@dhis2/ui'
+import { PropTypes } from '@dhis2/prop-types'
 import React from 'react'
 import i18n from '../locales'
 
@@ -9,17 +10,36 @@ const subscription = {
     pristine: true,
 }
 
-export const SaveCommandButton = () => {
-    const { submitting, pristine } = useFormState({ subscription })
+export const SaveCommandButton = ({ enabled }) => {
+    const { submitting, pristine } = useFormState({
+        subscription,
+    })
+    const disabled = pristine || submitting
 
     return (
         <Button
             primary
             type="submit"
-            disabled={pristine || submitting}
+            disabled={!enabled && disabled}
             icon={submitting ? <CircularLoader small /> : null}
         >
             {i18n.t('Save command')}
         </Button>
     )
+}
+
+SaveCommandButton.defaultProps = {
+    enabled: false,
+}
+
+SaveCommandButton.propTypes = {
+    /*
+     * This prop is used when fields, stored as array entries
+     * in the form state, are being removed while the form
+     * is still pristine.
+     *
+     * Form some reasons, RFF doesn't recognize this as a change
+     * and keeps the form state's pristine value `true`
+     */
+    enabled: PropTypes.bool,
 }
