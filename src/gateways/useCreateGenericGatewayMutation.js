b419b355c150b379cdcbd21e48f20d43be903cf6
Hendrik de Graaf <hendrik@dhis2.org>
2020-09-28 15:03:00 +0200

    fix(generic gateway): use additional fields in edit payload too

--- src/gateways/useCreateGenericGatewayMutation.js
+++ src/gateways/useCreateGenericGatewayMutation.js
@@ -1,52 +1,10 @@
 import { useDataMutation } from '@dhis2/app-runtime'
-
-/**
- * This the shape of the additional fields
- * that can be added to a generic config
- *
- * @typedef {Object} Parameter
- * @prop {string} key
- * @prop {string} value
- * @prop {bool} header
- * @prop {bool} encode
- * @prop {bool} confidential
- */
-
-/**
- * @param {Object} args
- * @param {string} args.configurationTemplate
- * @param {string} args.contentType
- * @param {string} args.name
- * @param {string} args.urlTemplate
- * @param {bool} args.sendUrlParameters
- * @param {bool} args.useGet
- * @param {Parameter[]} args.parameters
- *
- * @returns {Object}
- */
-const createDataFromVariables = ({
-    configurationTemplate,
-    contentType,
-    name,
-    parameters,
-    sendUrlParameters,
-    urlTemplate,
-    useGet,
-}) => ({
-    type: 'http',
-    configurationTemplate,
-    contentType,
-    name,
-    parameters,
-    sendUrlParameters,
-    urlTemplate,
-    useGet,
-})
+import { createGenericGateWayDataFromVariables } from './createGenericGateWayDataFromVariables'
 
 export const CREATE_GENERIC_GATEWAY_MUTATION = {
     resource: 'gateways',
     type: 'create',
-    data: createDataFromVariables,
+    data: createGenericGateWayDataFromVariables,
 }
 
 export const useCreateGenericGatewayMutation = () =>
