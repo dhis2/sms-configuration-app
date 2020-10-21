09f77a58dbe79918ec41de8bf0aca216aa9d3b56
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-10-21 09:58:41 +0200

    test(add commands): add cypress tests for add sms command form

---
+++ cypress/integration/commands/add_cmd_event_registration.feature
@@ -0,0 +1,29 @@
+Feature: The user can add a new event registration parser sms command
+
+    Scenario: The user successfully adds a new command
+        Given the user is adding a new event registration parser sms command
+        When the user enters the name
+        And the user chooses a program
+        And the user submits the form
+        Then the data should be sent successfully
+
+    Scenario: The user does not provide a name
+        Given the user is adding a new event registration parser sms command
+        When the user chooses a program
+        But the user leaves the name empty
+        And the user submits the form
+        Then the form should not submit
+        And display an error message on the name field
+
+    Scenario: The user does not provide a program
+        Given the user is adding a new event registration parser sms command
+        When the user enters the name
+        But the user leaves the program field empty
+        And the user submits the form
+        Then the form should not submit
+        And display an error message on the program field
+
+    Scenario: The user chooses a program
+        Given the user is adding a new event registration parser sms command
+        When the user chooses a program
+        Then a program stage should be selected automatically
---
+++ cypress/integration/commands/add_cmd_event_registration/index.js
@@ -0,0 +1,140 @@
+import { Before, Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+
+const programs = [
+    {
+        id: 'lxAQ7Zs9VYR',
+        displayName: 'Antenatal care visit',
+    },
+    {
+        id: 'kla3mAPgvCH',
+        displayName: 'Contraceptives Voucher Program',
+    },
+    {
+        id: 'q04UBOqq3rp',
+        displayName: 'Information Campaign',
+    },
+    {
+        id: 'eBAyeGv0exc',
+        displayName: 'Inpatient morbidity and mortality',
+    },
+    {
+        id: 'VBqh0ynB2wv',
+        displayName: 'Malaria case registration',
+    },
+    {
+        id: 'bMcwwoVnbSR',
+        displayName: 'Malaria testing and surveillance',
+    },
+    {
+        id: 'MoUd5BTQ3lY',
+        displayName: 'XX MAL RDT - Case Registration',
+    },
+]
+
+const programStages = [
+    {
+        id: 'dBwrot7S420',
+        displayName: 'Antenatal care visit - Program rules demo',
+    },
+]
+
+Before(() => {
+    cy.server()
+
+    cy.route({
+        url: /.*\/smsCommands/,
+        method: 'POST',
+        response: {
+            commands: [],
+        },
+    }).as('createSmsCommandXhr')
+
+    cy.route({
+        url: /.*\/programs/,
+        method: 'GET',
+        response: { programs },
+    }).as('programsXhr')
+
+    cy.route({
+        url: /.*\/programStages/,
+        method: 'GET',
+        response: { programStages },
+    }).as('programStagesXhr')
+})
+
+Given('the user is adding a new event registration parser sms command', () => {
+    cy.visitWhenStubbed('/')
+
+    cy.get('{navigation-navigationitem}:nth-child(3)').click()
+    cy.get('{views-smscommandlist-add}').click()
+    cy.get('{forms-fieldcommandparser-content}').click()
+    cy.get('[data-value="EVENT_REGISTRATION_PARSER"]').click()
+
+    cy.get('{forms-fieldcommandparser} [data-test="dhis2-uicore-select-input"]')
+        .invoke('text')
+        .should('equal', 'Event registration parser')
+})
+
+When('the user enters the name', () => {
+    cy.get('{commands-fieldcommandname} input').type('User name')
+})
+
+When('the user chooses a program', () => {
+    const program = programs[0]
+    cy.wrap(program).as('selectedProgram')
+
+    cy.get('{forms-fieldprogram}').click()
+    cy.get(`[data-value="${program.id}"]`).click()
+})
+
+When('the user submits the form', () => {
+    cy.get('{app} [type="submit"]').click()
+})
+
+When('the user leaves the name empty', () => {
+    cy.get('{commands-fieldcommandname} input').should('have.value', '')
+})
+
+When('the user leaves the program field empty', () => {
+    cy.get('{forms-fieldprogram} [data-test="dhis2-uicore-select-input"]')
+        .invoke('text')
+        .should('equal', '')
+})
+
+Then('the data should be sent successfully', () => {
+    cy.wait('@createSmsCommandXhr').then(xhr => {
+        expect(xhr.status).to.equal(200)
+    })
+})
+
+Then('the form should not submit', () => {
+    cy.get('{views-smscommandlist}').should('not.exist')
+})
+
+Then('display an error message on the name field', () => {
+    cy.get('{commands-fieldcommandname-validation}').should('exist')
+})
+
+Then('display an error message on the program field', () => {
+    cy.get('{forms-fieldprogram-validation}').should('exist')
+})
+
+Then('a program stage should be selected automatically', () => {
+    cy.all(
+        () => cy.get('@selectedProgram'),
+        () => cy.wait('@programStagesXhr')
+    ).then(([selectedProgram, programStagesXhr]) => {
+        expect(programStagesXhr.url).to.match(
+            new RegExp(`program.id:eq:${selectedProgram.id}`)
+        )
+
+        const attributedProgramStage =
+            programStagesXhr.response.body.programStages[0].displayName
+
+        cy.get(
+            '{forms-fieldprogramstage} [data-test="dhis2-uicore-select-input"]'
+        )
+            .invoke('text')
+            .should('equal', attributedProgramStage)
+    })
+})
