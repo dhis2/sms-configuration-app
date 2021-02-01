c9f70e0c23ef155e29db3a7007771f1a5d6c9639
Médi-Rémi Hashim <mediremi@gmail.com>
2021-02-01 13:56:08 +0000

    chore: add e2e tests for deleting sent messages

--- cypress/integration/sent/List/index.js
+++ cypress/integration/sent/List/index.js
@@ -1,23 +1,6 @@
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Then } from 'cypress-cucumber-preprocessor/steps'
 import { statusMap } from '../../../../src/views/sent_sms_list/translations'
-
-const apiRouteMatcher = {
-    method: 'GET',
-    path: /sms\/outbound/,
-}
-
-Given('there are no sent messages', () => {
-    cy.intercept(apiRouteMatcher, { fixture: 'sent/noSent' })
-})
-
-Given('some sent messages exist', () => {
-    cy.intercept(apiRouteMatcher, { fixture: 'sent/sent' })
-})
-
-Given('the user navigated to the sent messages page', () => {
-    cy.visit('/')
-    cy.get('{navigation-navigationitem}:nth-child(4)').click()
-})
+import '../common'
 
 Then('the user should be notified that there are no messages', () => {
     cy.get('[data-test="dhis2-uicore-tablecell"]').should(
@@ -32,12 +15,14 @@ Then('the sent messages are rendered as tabular data', () => {
 
 Then('each row displays the message contents and metadata', () => {
     cy.fixture('sent/sent').then(({ outboundsmss }) => {
-        outboundsmss.forEach(({ message, recipients, status }) => {
-            cy.get('[data-test="dhis2-uicore-tablerow"]').should($elem => {
-                expect($elem.text()).to.contain(message)
-                expect($elem.text()).to.contain(recipients.join(', '))
-                expect($elem.text()).to.contain(statusMap[status])
-            })
+        outboundsmss.forEach(({ message, recipients, status }, index) => {
+            cy.get(`[data-test="dhis2-uicore-tablerow"]:eq(${index})`).should(
+                $elem => {
+                    expect($elem.text()).to.contain(message)
+                    expect($elem.text()).to.contain(recipients.join(', '))
+                    expect($elem.text()).to.contain(statusMap[status])
+                }
+            )
         })
     })
 })
---
+++ cypress/integration/sent/common/index.js
@@ -0,0 +1,29 @@
+import { Before, Given } from 'cypress-cucumber-preprocessor/steps'
+
+export const apiRouteOptions = {
+    method: 'GET',
+    url: '**/sms/outbound**',
+}
+
+Before(() => {
+    cy.server()
+})
+
+Given('there are no sent messages', () => {
+    cy.route({
+        ...apiRouteOptions,
+        response: 'fixture:sent/noSent',
+    })
+})
+
+Given('some sent messages exist', () => {
+    cy.route({
+        ...apiRouteOptions,
+        response: 'fixture:sent/sent',
+    })
+})
+
+Given('the user navigated to the sent messages page', () => {
+    cy.visit('/')
+    cy.get('{navigation-navigationitem}:nth-child(4)').click()
+})
