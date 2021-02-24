4401064fb776e496e60431c8c466dfa699b3ca6d
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-02-24 10:55:58 +0100

    chore(network shim): enable networkshim setup

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,11 @@
 const {
     chromeAllowXSiteCookies,
     cucumberPreprocessor,
+    networkShim,
 } = require('@dhis2/cypress-plugins')
 
 module.exports = (on, config) => {
+    networkShim(on, config)
     chromeAllowXSiteCookies(on, config)
     cucumberPreprocessor(on, config)
 }
