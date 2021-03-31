915bfc33a52db8822a09b610f4dcaa9c6e212f35
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-03-31 16:08:01 +0200

    refactor: wrap completenessMethods in object

--- src/sms-command/components/FieldCompletenessMethod/completenessMethods.js
+++ src/sms-command/components/FieldCompletenessMethod/completenessMethods.js
@@ -1,16 +1,22 @@
 import i18n from '../../../locales'
 
-export const ALL_DATAVALUE = {
+const ALL_DATAVALUE = {
     value: 'ALL_DATAVALUE',
     label: i18n.t('Receive all data values'),
 }
 
-export const AT_LEAST_ONE_DATAVALUE = {
+const AT_LEAST_ONE_DATAVALUE = {
     value: 'AT_LEAST_ONE_DATAVALUE',
     label: i18n.t('Receive at least one data value'),
 }
 
-export const DO_NOT_MARK_COMPLETE = {
+const DO_NOT_MARK_COMPLETE = {
     value: 'DO_NOT_MARK_COMPLETE',
     label: i18n.t('Do not mark as complete'),
+}
+
+export const completenessMethods = {
+    ALL_DATAVALUE,
+    AT_LEAST_ONE_DATAVALUE,
+    DO_NOT_MARK_COMPLETE,
 }
