b2e3a7e626a1c9c4d172d04dbef044a8a740b438
Viktor Varland <viktor@dhis2.org>
2021-11-18 13:52:41 +0100

    chore(release): migrate to new app version scheme
    Align all our application versions that range between 0.x.x and 36.x.x
    with different semantics to a single version scheme with distinct
    semantics for what constitutes a breaking change.
    
    We have chosen to bump to v100.0.0 to signify the depature from the old
    version scheme to the new.
    
    For more information:
    dhis2/notes#293
    
    BREAKING CHANGE: App version becomes decoupled from DHIS2 versions, see
    the d2.config.js or App Hub for DHIS2 version compatibility.

--- d2.config.js
+++ d2.config.js
@@ -4,6 +4,8 @@ const config = {
     title: 'SMS Configuration',
     description: 'Configure SMS gateways and manage SMS messages',
     coreApp: true,
+    minDHIS2Version: '2.35',
+    id: '441cad0c-395a-4e93-8f10-475602397371',
 
     entryPoints: {
         app: './src/index.js',
