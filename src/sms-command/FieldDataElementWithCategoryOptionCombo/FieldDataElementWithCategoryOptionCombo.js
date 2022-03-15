c24a3b6566eb5d18a0f91411f0c593716a86fd50
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-03-15 16:52:10 +0100

    chore: remove commented out code

--- src/sms-command/FieldDataElementWithCategoryOptionCombo/FieldDataElementWithCategoryOptionCombo.js
+++ src/sms-command/FieldDataElementWithCategoryOptionCombo/FieldDataElementWithCategoryOptionCombo.js
@@ -101,7 +101,6 @@ FieldDataElementWithCategoryOptionCombo.propTypes = {
         id: PropTypes.string.isRequired,
     }).isRequired,
     categoryOptionCombo: PropTypes.shape({
-        // code: PropTypes.string.isRequired,
         displayName: PropTypes.string.isRequired,
         id: PropTypes.string.isRequired,
     }),
--- src/sms-command/FormKeyValueParser/Form.js
+++ src/sms-command/FormKeyValueParser/Form.js
@@ -132,7 +132,6 @@ Form.propTypes = {
                 id: PropTypes.string.isRequired,
             }).isRequired,
             categoryOptionCombo: PropTypes.shape({
-                // code: PropTypes.string.isRequired,
                 displayName: PropTypes.string.isRequired,
                 id: PropTypes.string.isRequired,
             }),
