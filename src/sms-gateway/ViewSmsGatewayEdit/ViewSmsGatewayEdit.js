eb986c330ebd29a2a3d75dae01e31ec06c7b4e95
Thomas Zemp <tom@dhis2.org>
2024-02-14 13:59:20 +0100

    fix: remove unnecessary explicit prop true value

--- src/sms-gateway/ViewSmsGatewayEdit/ViewSmsGatewayEdit.js
+++ src/sms-gateway/ViewSmsGatewayEdit/ViewSmsGatewayEdit.js
@@ -178,7 +178,7 @@ export const ViewSmsGatewayEdit = () => {
                                     ? history.push('/sms-gateway')
                                     : setShowCancelDialog(true)
                             }
-                            editMode={true}
+                            editMode
                         />
                     </div>
                 ) : (
